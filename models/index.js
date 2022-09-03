940d7c585f3b15c4dec4edf471786618e4d3ea11
musaliyah <asmusaliar@gmail.com>
2022-09-03 15:18:58 -0700

    belongsTo and hasMany methods added to index.js model file

--- models/index.js
+++ models/index.js
@@ -5,9 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  as: 'categoryProducts',
+  onDelete: 'SET NULL'
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
